c9dd78fde330115d245840be864acc56e829ef57
carpino <32466515+carpino@users.noreply.github.com>
2018-05-26 11:40:57 +0200

    GPS

--- app/src/main/java/com/example/amantova/wifip2pservice/IO/GpsStrategyClient.java
+++ app/src/main/java/com/example/amantova/wifip2pservice/IO/GpsStrategyClient.java
@@ -1,9 +1,42 @@
 package com.example.amantova.wifip2pservice.IO;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 public class GpsStrategyClient implements IOStrategy {
-    public void run(InputStream in, OutputStream out){
+
+    private static byte[] convertToByteArray(double value) {
+        byte[] bytes = new byte[8];
+        ByteBuffer buffer = ByteBuffer.allocate(bytes.length);
+        buffer.putDouble(value);
+        return buffer.array();
+    }
+    private static double toDouble(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getDouble();
+    }
+
+    public void run(final InputStream in, final OutputStream out, Activity main_act){
+
+        byte[] b1 = convertToByteArray(1);
+        byte[] b2 = convertToByteArray(2);
+
+        try {
+            in.read(b1);
+            in.read(b2);
+
+            Log.d("Longitude",String.valueOf(toDouble(b1)));
+            Log.d("Latitude", String.valueOf(toDouble(b2)));
+
+        }catch(IOException exc){
+            Log.d("IOEXCEPTION:", String.valueOf(exc.getCause()));
+        }
+
+
     };
 }
