bd18b4cad3d865f6a0694d6209b3b32a1ffcd241
carpino <32466515+carpino@users.noreply.github.com>
2018-05-30 14:55:17 +0200

    added get_list_for_service in packettable

--- app/src/main/java/com/example/amantova/wifip2pservice/routing/Packet_table.java
+++ app/src/main/java/com/example/amantova/wifip2pservice/routing/Packet_table.java
@@ -38,6 +38,17 @@ public class Packet_table {
   public List<Packet_table_item> get_packet_list(String eid_arg){
     return table.get(eid_arg);
   }
+  public List<Packet_table_item> get_list_for_service(String eid_arg, String service){
+    List<Packet_table_item> list_items = get_packet_list(eid_arg);
+    List<Packet_table_item> result_items = new LinkedList<>();
+    for(Packet_table_item item : list_items){
+      if(item.service.equals(service)){
+        result_items.add(item);
+      }
+    }
+    return result_items;
+  }
+
   public List<String> get_eids_list(){
     Set<String> eids = table.keySet();
     List<String> list_eids = new LinkedList<>();
